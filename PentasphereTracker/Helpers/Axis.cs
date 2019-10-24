6090f73ee004b8ee9fa6d202ed7dd437a9e2763f
cmhuggins <4612427+cmhuggins@users.noreply.github.com>
2019-10-24 11:29:52 -0500

    Parse input string finished

--- PentasphereTracker/Helpers/Axis.cs
+++ PentasphereTracker/Helpers/Axis.cs
@@ -1,4 +1,7 @@
 namespace PentasphereTracker.Helpers {
+	/// <summary>Enum <c>Axis</c>
+	/// contains each travelable axis in a 5D sphere
+	/// </summary>
 	public enum Axis {
 		NorthSouth = 1,
 		EastWest = 2,
--- PentasphereTracker/Helpers/PolarCoord.cs
+++ PentasphereTracker/Helpers/PolarCoord.cs
@@ -1,10 +1,19 @@
 using System;
 
 namespace PentasphereTracker.Helpers {
+	/// <summary>Class <c>PolarCoord</c>
+	/// maps the 3D object's position by axis
+	/// </summary>
 	public struct PolarCoord {
+		// Hold the axis of which this PolarCoord represents
 		public Axis GetAxis { get; }
+		// Hold the actual polar coordinate for use
 		public double Degree { get; }
 		
+		/// <summary>Constructor <c>PolarCoord</c>
+		/// creates a holder for the polar coordinate for use elsewhere
+		/// </summary>
+		/// <param name="axis">The axis of which this polar coordinate exists</param>
 		public PolarCoord(Axis axis) {
 			GetAxis = axis;
 
