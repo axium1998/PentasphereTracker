3c3d743038441c616c24408cbbe7192e3c10e6a0
cmhuggins <4612427+cmhuggins@users.noreply.github.com>
2019-10-25 07:31:31 -0500

    dev-20192510073150

--- PentasphereTracker/Program.cs
+++ PentasphereTracker/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Net.Mime;
 using System.Text.RegularExpressions;
 using PentasphereTracker.Helpers;
 
@@ -16,7 +17,9 @@ namespace PentasphereTracker {
         
         static void Main(string[] args) {
             // For as long as we need, loop my basic input handler
-            while (true) {
+            var traveling = true;
+            while (traveling) {
+                Console.Clear();
                 try {
                     // Everytime we are able to move, we need our current coords
                     Console.WriteLine("{0,9:0.0000}\tN/S", _northSouth.Degree);
@@ -24,14 +27,23 @@ namespace PentasphereTracker {
                     Console.WriteLine("{0,9:0.0000}\tA/K", _anaKata.Degree);
                     Console.WriteLine("{0,9:0.0000}\tS/C", _strangeCharm.Degree);
                     /* Input is in this format:
-                     * <Distance> <Angle to axis plane><Axis traveled upon> [<Angle for second axis of travel><Axis traveled upon>]
+                     * <Distance> <Angle in coordinate form> <Angle to axis plane><Axis traveled upon>
+                     * [<Angle for second axis of travel><Axis traveled upon>]
                      */
-                    var input = Console.ReadLine()
-                                       ?.Trim();
-                    //REF: 56 30NE 64CA
+                    
+                    //REF: 56 45 30NE 64CA
                     //REF: 56 meters, 30 degrees North of East, 64 degrees Charm of Ana 
                     // If our input was null, try again
-                    if (input == null) continue;
+                    var input = Console.ReadLine()
+                                       ?.Trim();
+                    switch (input) {
+                        case "":
+                            break;
+                        case "break":
+                            Environment.ExitCode = 0;
+                            traveling = false;
+                            break;
+                    }
 
                     var inputArray = new List<object>{};
                     // Since each operator in the input is separated by a space, split on that to isolate variables
@@ -39,35 +51,57 @@ namespace PentasphereTracker {
 
                     // If we can't parse the distance, the input was incorrect
                     if (!double.TryParse(inputs[0], out var distance))
-                        throw new ArgumentException($"{distance} is not a vaild distance");
+                        throw new ArgumentException($"{inputs[0]} is not a valid distance");
+                    
+                    if(!double.TryParse(inputs[1], out var angle))
+                        throw new ArgumentException($"{inputs[1]} is not a valid angle");
                     
                     // Since it checks out, add it to my list
                     inputArray.Add(distance);
+                    inputArray.Add(angle);
                     
                     // Now we have it, remove it from the input
                     input = input.Replace(inputs[0], "").Trim();
+                    input = input.Replace(inputs[1], "").Trim();
                     
                     // Now all we're left with is 1 or 2 directional vectors, let's do a thing with them
                     inputArray.AddRange(input.Trim().Split(" ").Select(s => new DirectionMovement(s)));
 
+                    List<DirectionMovement> directionMovements = new List<DirectionMovement>() {
+                        (DirectionMovement) inputArray[2],
+                        (DirectionMovement) inputArray[3]
+                    };
+
+                    // Alright, time to do some trig!!!
+                    
+
 #if DEBUG
                     // Because I myself placed the variables in each slot of the List, I can do this without checking
                     Console.WriteLine($"Distance: {(double)inputArray[0]}");
-                    if (inputArray.Count == 2) {
-                        var angle = (DirectionMovement) inputArray[1];
-                        Console.WriteLine($"Direction of movement: {angle.Degree} degrees {angle.Angle1} of {angle.Angle2}");
-                    } else {
-                        var angle1 = (DirectionMovement) inputArray[1];
-                        var angle2 = (DirectionMovement) inputArray[2];
+                    if (inputArray.Count == 3) {
+                        var degree = (DirectionMovement) inputArray[1];
+                        Console.WriteLine($"Distance: {distance}");
+                        Console.WriteLine($"Angle: {angle}");
+                        Console.WriteLine($"Direction of movement: {degree.Degree} degrees {degree.Angle1} of {degree.Angle2}");
+
+                        Console.ReadKey();
+                    } else if (inputArray.Count == 4) {
+                        var angle1 = (DirectionMovement) inputArray[3];
+                        var angle2 = (DirectionMovement) inputArray[4];
                         Console.WriteLine($"First direction of movement: {angle1.Degree} degrees {angle1.Angle1} of {angle1.Angle2}");
                         Console.WriteLine($"Second direction of movement: {angle2.Degree} degrees {angle2.Angle1} of {angle2.Angle2}");
+
+                        Console.ReadKey();
+                    } else {
+                        Console.WriteLine("Invalid amount of arguments");
+
+                        Console.ReadKey();
                     }
 #endif
-
-                    Console.ReadKey();
-                    break;
                 } catch(ArgumentException e) { Console.WriteLine($"{e.Message}");}
             }
+            
+            Environment.Exit(Environment.ExitCode);
         }
     }
 }
